cc98a829d66d14fd50f9d48a951113e6a1d159f7
Gabriel <GabrielTeixeira28>
2020-08-07 21:05:11 -0300

    Dia 3 NLW

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -2,11 +2,11 @@ import React from 'react';
 
 import Routes from './routes';
 
-import './assets/styles/global.css';
+import './assets/styles/global.css'
 
 function App() {
   return (
-   <Routes />
+    <Routes />
   );
 }
 
--- web/src/components/TeacherItem/index.tsx
+++ web/src/components/TeacherItem/index.tsx
@@ -2,37 +2,56 @@ import React from 'react';
 
 import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
 
-import './styles.css';
+import './styles.css'
+import api from '../../services/api';
 
-function TeacherItem() {
-    return(
-        <article className="teacher-item">
-            <header>
-                <img src="https://s1.static.brasilescola.uol.com.br/be/conteudo/images/a-quimica-oferece-conhecimentos-muito-importantes-para-desenvolvimento-nossa-sociedade-562fd9fc8296e.jpg" alt="Professor"/>
-                <div>
-                    <strong>Diego Fernandes</strong>
-                    <span>Química</span>
-                </div>
-            </header>
+export interface Teacher {
+  id: number;
+  avatar: string;
+  bio: string;
+  cost: number;
+  name: string;
+  subject: string;
+  whatsapp: string;
+}
 
-            <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit fugiat quas unde magnam corporis eum omnis. 
-                <br /><br />
-                Saepe mollitia quasi repellat veritatis, vitae, soluta aperiam ut voluptatem sunt deserunt eligendi aliquam.
-            </p>
+interface TeacherItemProps {
+  teacher: Teacher;
+}
 
-            <footer>
-                <p>
-                    Preço/hora
-                    <strong>R$ 80,00</strong>
-                </p>
-                <button type="button">
-                <img src={whatsappIcon} alt="Professor"/>
-                    Entrar em contato
-                </button>
-            </footer>
-        </article>
-    );
+const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
+  function createNewConnection() {
+    api.post('connections', {
+      user_id: teacher.id
+    });
+  }
+  
+  return (
+    <article className="teacher-item">
+      <header>
+        <img src={teacher.avatar} alt={teacher.name} />
+        <div>
+          <strong>{teacher.name}</strong>
+          <span>{teacher.subject}</span>
+        </div>
+      </header>
+
+      <p>
+        {teacher.bio}
+      </p>
+
+      <footer>
+        <p>
+          Preço/hora
+  <strong>R$ {teacher.cost}</strong>
+        </p>
+        <a target="_blank" onClick={createNewConnection} href={`https://wa.me/${teacher.whatsapp}`} type="button">
+          <img src={whatsappIcon} alt="Whatsapp" />
+        Entrar em contato
+      </a>
+      </footer>
+    </article>
+  );
 }
 
 export default TeacherItem;
